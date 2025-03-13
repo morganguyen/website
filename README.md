4b507ff92c22013d0c7d5273133f96f089f4ee4c
Damian Vu <contact@damianvu.com>
2025-03-13 08:56:33 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+# minhtrucnguyen.com
+
+- Uses Svelte 5 and SvelteKit
+- Hosted using cloudflare pages
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+
+<div class="container mx-auto flex flex-col gap-8">
+    <h1>Welcome to Morgan's Website!</h1>
+    <p>Check back soon for cool stuff!</p>
+</div>
